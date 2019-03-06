2b7f3bdddba0eecd0bb8041296ac9a3f80d0cac8
ciano <ciano@CiansPC>
2019-03-06 16:33:18 +0000

    Cian Refactoring 2

---
+++ Java_Project/src/businessLogic/Meal.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package businessLogic;
+
+/**
+ *
+ * @author ciano
+ */
+public class Meal {
+    //Declare properties
+    private int id, calories;
+    private String mealName, contents, category;
+    private Boolean containsNuts, vegatrain, vegan, glutenFree;
+    private double price;
+    
+    //Create setter and getters
+    public void setID(int id)
+    
+}
