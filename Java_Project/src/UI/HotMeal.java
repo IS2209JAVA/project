eb47658afa63e4a8f39a2b3dde406bc324312635
asus <asus@DESKTOP-AU4KM9T>
2019-03-08 15:23:25 +0000

    LI

---
+++ Java_Project/src/UI/HotMeal.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package UI;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author asus
+ */
+public class HotMeal {
+    public static void start() {
+        GridPane allMenuForm = new GridPane();
+        allMenuForm.setAlignment(Pos.CENTER);
+        
+        //Just something to put in the form
+        Button backBtn = new Button("Back");
+        allMenuForm.add(backBtn,0,0);
+   
+        backBtn.setOnAction(new EventHandler<ActionEvent>() {
+            
+            @Override
+            public void handle(ActionEvent event) {
+                //I don't know how to return to primary stage.
+            }
+        });
+        
+        //Create the scene
+        Scene myScene = new Scene(allMenuForm,800,650);
+        Stage primaryStage = new Stage();
+        primaryStage.setTitle("HotMeal");
+        primaryStage.setScene(myScene);
+        primaryStage.show();
+    }
+}
